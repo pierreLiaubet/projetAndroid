f8ce048b9297cfaf6518f9afc2ea2523216e8cf7
Pierrot <pierrotl15@gmail.com>
2016-03-21 18:04:54 +0100

    Client serveur ?

--- BluetoothTalkie/app/src/main/java/com/pierreliaubet/bluetoothtalkie/model/Server.java
+++ BluetoothTalkie/app/src/main/java/com/pierreliaubet/bluetoothtalkie/model/Server.java
@@ -1,7 +1,59 @@
 package com.pierreliaubet.bluetoothtalkie.model;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.util.UUID;
+
 /**
  * Created by pierreliaubet on 18/03/2016.
  */
-public class Server {
+public class Server extends Thread {
+    public BluetoothAdapter btAdpt;
+    private UUID uUID;
+    private final BluetoothServerSocket mmServerSocket;
+    public Server(UUID uuid) {
+        // Use a temporary object that is later assigned to mmServerSocket,
+        // because mmServerSockets is final
+        BluetoothServerSocket tmp = null;
+        try {
+            // MY_UUID is the app's UUID string, also used by the client code
+            tmp = btAdpt.listenUsingRfcommWithServiceRecord("serveurTalkie",uUID);
+        } catch (IOException e) { }
+        mmServerSocket = tmp;
+    }
+
+    public void run() {
+        BluetoothSocket socket = null;
+        // Keep listening until exception occurs or a socket is returned
+        while (true) {
+            try {
+                socket = mmServerSocket.accept();
+            } catch (IOException e) {
+                break;
+            }
+            // If a connection was accepted
+            if (socket != null) {
+                // Do work to manage the connection (in a separate thread)
+
+                //Surement TheConnectedThread.run, mais pas sûr
+                manageConnectedSocket(socket); //Normal que ce soit rouge pour l'instant
+                try {
+                    mmServerSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    /** Will cancel the listening socket, asnd cause the thread to finish */
+    public void cancel() {
+        try {
+            mmServerSocket.close();
+        } catch (IOException e) { }
+    }
 }
